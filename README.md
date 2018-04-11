52dda29167bff7b508ddb5c4d48fda01fb20d2d0
maxime pierre <maxime.pierre@webmediasolutionz.com>
2018-04-11 02:33:06 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Diply Coding Test
+
+## Step 1: Install Node, NPM
+
+Install Node and NPM on your machine; this typically requires administrative access, 
+e.g. become root or use `sudo` (or use a Windows installer).
+
+You must be running at least Node 4.x.x and npm 3.x.x. You can verify 
+what, if any, versions you have installed by running `node -v` and 
+`npm -v` in a terminal window.
+
+You can download current versions of these tools [here](https://nodejs.org/en/download/current/).
+
+If you use a Linux-like environment, you are encouraged to use your
+package manager to install these tools.
+
+If you use OS X, you may wish to consider installing via [HomeBrew](http://brew.sh/).
+
+## Step 2: Install the NPM dependencies
+
+`$ npm install`
+
+## Step 3: Development server
+
+Run `$ npm start` for a dev server. Navigate to `http://localhost:3000/`. The app will automatically reload if you change any of the source files.
+
+## Step 4: Build
+
+Run `$ npm run build` to build the project. The build artifacts will be stored in the `dist/` directory.
---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+// Importing Packages
+const gulp = require('gulp');
+const uglify = require('gulp-uglify');
+const sass = require('gulp-sass');
+const browerSync = require('browser-sync').create();
+
+// Copy all HTML files
+gulp.task(`copyHtml`, () => {
+    gulp.src('./src/*.html')
+        .pipe(gulp.dest('./dist'))
+        .pipe(browerSync.stream());
+});
+
+// Minify JS
+gulp.task('minify', () => {
+    gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/scripts/'))
+        .pipe(browerSync.stream());
+});
+
+// Compile Sass
+gulp.task('sass', () => {
+    gulp.src('./src/stylesheets/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./dist/stylesheets'))
+        .pipe(browerSync.stream());
+});
+
+// All tasks
+gulp.task('all', ['copyHtml', 'minify', 'sass']);
+
+// Serve files
+gulp.task('serve', ['all'], () => {
+    browerSync.init({
+        server: './dist'
+    });
+
+    // watching files for changes
+    gulp.watch('./src/*.html', ['copyHtml']);
+    gulp.watch('./src/*.html').on('change', browerSync.reload);
+    gulp.watch('./src/scripts/*.js', ['minify']);
+    gulp.watch('./src/stylesheets/*.scss', ['sass']);
+});
+
+// Default task
+gulp.task('default', ['serve']);
